7b3a133c06683e4b2589dc2a9e10cf18528b40e7
unknown <kostya.mkhalkov@bk.ru>
2024-04-30 14:51:20 +0300

    feat: added classes for Navigation components

--- src/components/navigation/js/navigation.js
+++ src/components/navigation/js/navigation.js
@@ -1,7 +1,8 @@
 import '../css/navigation.css'
 export default function Navigation({onClicked, className}) {
+    const combinedClass = `header-nav ${className}`;
     return (
-        <div className={className}>
+        <div className={combinedClass}>
             <button className="btn-nav" onClick={() => onClicked('coffe house')}>Coffe house</button>
             <button className="btn-nav" onClick={() => onClicked('our coffee')}>Our coffee</button>
             <button className="btn-nav" onClick={() => onClicked('for your pleasure')}>For your pleasure</button>
