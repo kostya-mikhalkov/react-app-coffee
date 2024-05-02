482123d2cb49515778cc7e1f5d8bb04285457226
unknown <kostya.mkhalkov@bk.ru>
2024-05-02 10:10:12 +0300

    feat: complete aboutUs section

--- src/components/coffee-house/js/coffeeHouse.js
+++ src/components/coffee-house/js/coffeeHouse.js
@@ -1,9 +1,13 @@
 import { Component } from "react";
 
 import HeaderCoffeeHouse from "../blocks/header/js/headerCoffeeHouse";
+import AboutUs from "../blocks/about-us/js/aboutUs";
 
 export default function CoffeeHouse() {
     return (
-        <HeaderCoffeeHouse />
+        <>
+            <HeaderCoffeeHouse />
+            <AboutUs />
+        </>
     )
 }
