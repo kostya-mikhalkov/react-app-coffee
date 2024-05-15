e1c42f2cb3453d69f0a8e0d435ebb8ac133fce66
unknown <kostya.mkhalkov@bk.ru>
2024-05-15 10:11:26 +0300

    feat: deploy project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import {Component} from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import CoffeeHouse from './components/coffee-house/js/coffeeHouse';
 import OurCoffee from './components/our-coffee/js/ourCoffee';
 import Pleasure from './components/pleasure/js/pleasure';
@@ -8,7 +8,7 @@ import Pleasure from './components/pleasure/js/pleasure';
 class App extends Component {
  render() {
   return(
-    <Router>
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<CoffeeHouse />} />
         <Route path="/coffee-house" element={<CoffeeHouse />} />
